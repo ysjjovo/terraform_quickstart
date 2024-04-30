ec47b1e43c24cf631e343abd1e999f3e4ef73473
ysjjovo <ysjjovo@163.com>
2024-04-30 17:45:47 +0800

    aws ec2

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
-resource "null_resource" "default" {
-  provisioner "local-exec" {
-    command = "echo 'Hello World'"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06d5c50c30a35fb88"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
   }
 }
